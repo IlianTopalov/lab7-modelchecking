d782ba6ac0e6ebf64385e810effcc2d620d744b2
Nicolas Junod <nicolas.junod@elca.ch>
2025-01-22 11:44:42 +0100

    Buffer Opti not finished yet

--- code/tests/main.cpp
+++ code/tests/main.cpp
@@ -1,11 +1,13 @@
 
 #include "modeltemplate.h"
 #include "modelnumbers.h"
+#include "modelbuffernimpostor.h"
+#include "modelbuffernopti.h"
 #include "pcomodelchecker.h"
 
 #include <pcosynchro/pcomanager.h>
 
-int main(int /*argc*/, char */*argv*/[])
+int main(int argc, char *argv[])
 {
     // Uncommenting the following line allows to easily observe the PcoManager in the debugger
     //    auto pcoManager = PcoManager::getInstance();
@@ -18,12 +20,25 @@ int main(int /*argc*/, char */*argv*/[])
         // checker.run();
     }
     {
-        ModelNumbers model;
+        // ModelNumbers model;
+        // PcoModelChecker checker;
+        // checker.setModel(&model);
+        // checker.run();
+    }
+    if (argc == 1 || argc == 2 && argv[1] == "opti" )
+    {
+        ModelProdConsOpti model;
         PcoModelChecker checker;
         checker.setModel(&model);
         checker.run();
     }
-
+    if (argc == 1 || argc == 2 && argv[1] == "impostor" )
+    {
+        ModelProdConsImpostor model;
+        PcoModelChecker checker;
+        checker.setModel(&model);
+        checker.run();
+    }
 
     return 0;
 }
--- code/tests/modelbuffernopti.h
+++ code/tests/modelbuffernopti.h
@@ -9,9 +9,18 @@
 
 #include "abstractbuffer.h"
 
-class ThreadProducer : public ObservableThread {  // TODO Extend AbstractBuffer
+class ThreadProdCon : public ObservableThread, public AbstractBuffer<int> {
 public:
-    explicit ThreadProducer(std::string id = "") : ObservableThread(std::move(id)) {
+    int depth() {
+        return this->scenarioSize;
+    }
+
+    explicit ThreadProdCon(std::string id = "") : ObservableThread(id) {
+        std::string lowerId = id;
+        std::transform(lowerId.begin(), lowerId.end(), lowerId.begin(), ::tolower);
+        if (lowerId.compare(0, 4, "prod") == 0) {
+            isProd = true;
+        }
         scenarioGraph = std::make_unique<ScenarioGraph>();
     }
 
@@ -22,47 +31,109 @@ public:
 
     }
 
+    void put(int item) override {
+        mutex.get()->acquire();
+        if (nbElements == bufferSize) {
+            nbWaitingProd += 1;
+            mutex.get()->release();
+            waitProd.get()->acquire();
+        }
+        elements[writePointer] = item;
+        writePointer = (writePointer + 1)
+                       % bufferSize;
+        nbElements ++;
+        if (nbWaitingConso > 0) {
+            nbWaitingConso -= 1;
+            waitConso.get()->release();
+        }
+        else {
+            mutex.get()->release();
+        }
+    }
+
+    int get(void) override {
+        int item;
+        mutex.get()->acquire();
+        if (nbElements == 0) {
+            nbWaitingConso += 1;
+            mutex.get()->release();
+            waitConso.get()->acquire();
+        }
+        item = elements[readPointer];
+        readPointer = (readPointer + 1)
+                      % bufferSize;
+        nbElements --;
+        if (nbWaitingProd > 0) {
+            nbWaitingProd -= 1;
+            waitProd.get()->release();
+        }
+        else {
+            mutex.get()->release();
+        }
+        return item;
+    }
+
 private:
-    static std::unique_ptr<PcoSemaphore> semaphore;
-    void run() override {
+     bool isProd = false;
+    int scenarioSize = 0;
+    static std::unique_ptr<PcoSemaphore> mutex;
+    static std::unique_ptr<PcoSemaphore> waitProd;
+    static std::unique_ptr<PcoSemaphore> waitConso;
+    std::vector<int> elements;
+    int writePointer, readPointer, nbElements, bufferSize;
+    unsigned nbWaitingProd, nbWaitingConso;
+
+   void run() override {
+        int item;
+        if (isProd) {
+            item = 1;
+            put(item);
+        } else {
+            item = get();
+        }
 
     }
 };
 
-class ThreadConsumer : public ObservableThread {
-public:
-    explicit ThreadConsumer(std::string id = "") : ObservableThread(std::move(id)) {
-        scenarioGraph = std::make_unique<ScenarioGraph>();
-    }
+std::unique_ptr<PcoSemaphore> ThreadProdCon::mutex = std::make_unique<PcoSemaphore>(0);
+std::unique_ptr<PcoSemaphore> ThreadProdCon::waitProd = std::make_unique<PcoSemaphore>(0);
+std::unique_ptr<PcoSemaphore> ThreadProdCon::waitConso = std::make_unique<PcoSemaphore>(0);
 
-    static void init() {
-
-    }
-
-    static void clean() {
-
-    }
-
-private:
-    static std::unique_ptr<PcoSemaphore> semaphore;
-    void run() override {
-
-    }
-};
-
+template <typename T>
 class ModelProdConsOpti : public PcoModel {
+    ModelProdConsOpti(int size, int scenarioAmount = 1) : elements(size), elementsCapacity(size), scenarioAmount(scenarioAmount),
+                                                            writePointer(0), readPointer(0), nbElements(0), bufferSize(size),
+                                                            mutex(1), waitProd(0), waitConso(0), nbWaitingProd(0), nbWaitingConso(0) {
+    }
+
     bool checkInvariants() override {
         return true;
     }
-    
-    
+
+
     void build() override {
-        scenarioBuilder = std::make_unique<ScenarioBuilderBuffer>();
-        scenarioBuilder->init(threads, 9);  // TODO Change recursion depth ?
+        int depth = 0;
+        // Create threads for our scenario
+        // threads.emplace_back(std::make_unique<ThreadProducer>("Prod1"));
+        threads.emplace_back(std::make_unique<ThreadProdCon>("Prod1"));
+        // threads.emplace_back(std::make_unique<ThreadConsumer>("Cons1"));
+        // Get the depth of the scenario
+        // Would have been cleaner to have some sort of intermediate class with virtual depth()
+        // to handle this but threads (from PcoModel) is already <ObservableThread>
+        for (auto &t : threads) {
+            if(auto *producer = dynamic_cast<ThreadProducer *>(t.get()))
+                depth += producer->depth();
+            else if(auto *consumer = dynamic_cast<ThreadConsumer *>(t.get()))
+                depth += consumer->depth();
+        }
+        // If scenarioAmount = 1 => generates all scenarios
+        scenarioBuilder = std::make_unique<ScenarioBuilderBuffer>(scenarioAmount);
+        scenarioBuilder->init(threads, depth);
     }
 
     void preRun(Scenario& /*scenario*/) override {
-
+        elements.clear();
+        elements.reserve(elementsCapacity); // In case something broke
     }
 
     void postRun(Scenario &scenario) override {
@@ -77,6 +148,15 @@ class ModelProdConsOpti : public PcoModel {
         std::cout << std::endl;
         std::cout << std::flush;
     }
+
+    private:
+        int scenarioAmount;
+        int elementsCapacity;
+        std::vector<T> elements;
+
+        int writePointer, readPointer, nbElements, bufferSize;
+        PcoSemaphore mutex, waitProd, waitConso;
+        unsigned nbWaitingProd, nbWaitingConso;
 };
 
 #endif // MODELBUFFERNOPTI_H
